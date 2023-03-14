b7a33dca32d823379ff2ce3b80e7f68637b98d7b
EvaRadu <evaradu@hotmail.fr>
2023-03-14 14:56:41 +0100

    Dependency Injection

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
-import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
 import { IEvent } from './shared/index';
 
@@ -12,7 +11,7 @@ import { IEvent } from './shared/index';
         <hr/>
         <div class="row">
             <div class="col-md-5" *ngFor="let event of events">
-                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event" ></event-thumbnail>
+                <event-thumbnail [event]="event" ></event-thumbnail>
             </div>
         </div>
     </div>
@@ -23,7 +22,6 @@ export class EventsListComponent implements OnInit {
     events: IEvent[]
 
     constructor(private eventService: EventService, 
-        private toastr: ToastrService,
         private route: ActivatedRoute){
 
     }
@@ -32,8 +30,5 @@ export class EventsListComponent implements OnInit {
         this.events = this.route.snapshot.data['events'];
     }
 
-    handleThumbnailClick(eventName){
-        this.toastr.success(eventName)
-    }
-    
+
 }
