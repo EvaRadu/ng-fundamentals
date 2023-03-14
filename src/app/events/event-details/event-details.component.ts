a2c30f2ae10e08cf9ac4bb28b283c396c95df2a3
EvaRadu <evaradu@hotmail.fr>
2023-03-14 09:49:37 +0100

    forms

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '../shared/index';
 
 @Component({
     selector: 'events-app',
@@ -14,7 +15,7 @@ import { ActivatedRoute } from '@angular/router';
 
 export class EventDetailsComponent {
 
-    event: any
+    event: IEvent
 
     constructor(private eventService: EventService, private route : ActivatedRoute){
 
