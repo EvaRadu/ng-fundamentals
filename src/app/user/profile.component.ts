780c347abcd70a1f4f03c40cac26d9218f76ad1c
EvaRadu <evaradu@hotmail.fr>
2023-03-15 10:52:07 +0100

    authentification

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -46,7 +46,9 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues){
     if(this.profileForm.valid){
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.toastr.success('Profile Saved')
+      .subscribe(() => {
+        this.toastr.success('Profile Saved')
+      })
     }
   }
 
@@ -56,6 +58,12 @@ export class ProfileComponent implements OnInit {
 
   validateLastName(){
     return this.lastName.valid || this.lastName.untouched
+  }
+
+  logout(){
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/user/login']);
+    })
   }
 
   
